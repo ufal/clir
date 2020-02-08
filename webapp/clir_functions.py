dec9d20bfd6f9dece0c9bf8155bbafc1556a6f84
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2020-02-08 17:02:44 +0100

    style

--- webapp/clir_functions.py
+++ webapp/clir_functions.py
@@ -2,26 +2,115 @@
 #coding: utf-8
 
 import requests
+import json
 
-class results:
+class Results:
     def __init__(self, response):
         self.response = response
-        j = response.json()
-        self.numFound = j['response']['numFound']
-        self.docs = j['response']['docs']
-        self.hl = j['highlighting']
+        self.json = response.json()
+        self.numFound = self.json['response']['numFound']
+        
+        docs = self.json['response']['docs']
+        hl = self.json['highlighting']
+        
+        self.results = list()
+        for i, doc in enumerate(docs):
+            doc_hl = hl[doc['id']]
+            self.results.append(Result(doc, doc_hl))
+
+    def debugprint(self):
+        print('<hr>')
+        print('<pre>')
+        print(json.dumps(self.json, indent=4))
+        print('</pre>')
+        print('<hr>')
+
+class Result:
+    def __init__(self, doc, doc_hl):
+        self.doc = doc
+        if 'content' in doc:
+            self.content = doc['content'][0]
+        else:
+            self.content = str(doc)
+        if 'content' in doc_hl:
+            self.hl = doc_hl['content'][0]
+        else:
+            self.hl = None
+        self.docid = doc['id']
+        if '/data/' in self.docid:
+            # this hopefully means it is a document with our special ID # strucure
+            self.info = Result.parse_id(self.docid)
+            self.metadata = Result.get_metadata(self.info)
+        else:
+            self.info = None
+            self.metadata = None
+
+    def parse_id(docid):
+        # e.g. /data/data_cs/source_fr/nku_be/2020/2020_02_report.txt
+        prefix = docid.find('/data/')
+        assert prefix != -1
+        parts = docid[prefix+1:].split('/', 6)
+        assert len(parts) == 6
+        # data
+        assert parts[0] == 'data'
+        info = {}
+        # data_cs
+        assert len(parts[1]) == 7 and parts[1].startswith('data_')
+        info['lang'] = parts[1][-2:]
+        # source_fr
+        assert len(parts[2]) == 9 and parts[2].startswith('source_')
+        info['src'] = parts[2][-2:]
+        # nku_be
+        assert len(parts[3]) == 6 and parts[3].startswith('nku_')
+        info['nku'] = parts[3][-2:]
+        # 2020
+        assert len(parts[4]) == 4 and parts[4].isdecimal()
+        info['year'] = int(parts[4])
+        # 2020_02_report.txt
+        assert parts[5].endswith('.txt')
+        info['filename'] = parts[5][:-4]
+        return info
+
+
+    def get_metadata(info):
+        return None
+
+
+    def show(self, C):
+        print('<div class="result" id="' + self.docid + '">')
+        if self.metadata:
+            C.print_h2(self.metadata['name'])
+        elif self.info:
+            C.print_h2(self.info['filename'])
+        else:
+            C.print_h2(self.docid)
+        if self.hl:
+            C.print_hl(self.hl)
+        else:
+            if len(self.content) < C.LIMIT:
+                C.print_p(self.content)
+            else:
+                C.print_p(self.content[:C.LIMIT] + '...')
+        print('</div>')
 
 class CLIR:
 
     texts = {
             'CLIR results': {'cs': 'výsledky CLIR',},
             'Results for query': {'cs': 'Výsledky pro dotaz',},
+            'Number of results found': {'cs': 'Počet nalezených výsledků',},
+            'CLIR: no query was specified': {'cs': 'CLIR: nebyl zadán žádný dotaz',},
+            '': {'cs': '',},
+            '': {'cs': '',},
+            '': {'cs': '',},
             
             
             }
 
-    def __init__(self, language):
+    def __init__(self, language, url):
         self.language = language
+        self.url = url
+        self.LIMIT = 200
 
     def t(self, text):
         if text in CLIR.texts and self.language in CLIR.texts[text]:
@@ -35,9 +124,7 @@ class CLIR:
         <html>
         <head>
             <title>''' + title + '''</title>
-            <style>
-                em {font-weight: bold}
-            </style>
+            <link rel="stylesheet" type="text/css" href="http://ufallab.ms.mff.cuni.cz/~rosa/elitr/clir.css">
         </head>
 
         <body>
@@ -52,17 +139,27 @@ class CLIR:
     def print_h1(self, text):
         print('<h1>' + text + '</h1>')
 
+    def print_h2(self, text):
+        print('<h2>' + text + '</h2>')
+
     def print_p(self, text):
         print('<p>' + text + '</p>')
 
+    def print_hl(self, text):
+        print('<div class="hl">' + text + '</div>')
+
     def get_results(self, q):
-        url = 'http://sol2:8989/solr/techproducts/select'
         data = {'q': q,
                 'hl': 'true', # highlighting
                 'hl.fl' : 'content', # what to highlight
                 }
         # highlighting->id->content[0] ... <em> highlights search query
-        response = requests.get(url, data = data)
+        response = requests.get(self.url, data = data)
         #response.encoding='utf8'
-        return results(response)
+        return Results(response)
+
+    def show_results(self, results):
+        for result in results.results:
+            result.show(self)
+
 
--- webapp/vystup.py
+++ webapp/vystup.py
@@ -3,15 +3,12 @@
 import sys
 import io
 import os
-import json
 from urllib.parse import parse_qs
 from clir_functions import CLIR
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-LIMIT = 200
-
-C = CLIR('cs')
+C = CLIR(language = 'cs', url = 'http://sol2:8989/solr/techproducts/select')
 
 qs = parse_qs(os.environ['QUERY_STRING']) if 'QUERY_STRING' in os.environ else {}
 if 'q' in qs:
@@ -19,32 +16,11 @@ if 'q' in qs:
     C.print_header(title = q + ' - ' + C.t('CLIR results') )
     C.print_h1(C.t('Results for query') + ' ' + q)
     results = C.get_results(q)    
-    C.print_p('Number of results found: ' + str(results.numFound))
-    for i, doc in enumerate(results.docs):
-        docid = doc['id']
-        print('<br>')
-        print('Result', i+1, 'is document ID', docid, end='<br>')
-        hl = results.hl[docid]
-        if 'content' in hl:
-            for item in hl['content']:
-                print('...', item, '...', end='<br>')
-        if 'content' in doc:
-            sd = str(doc['content'])
-        else:
-            sd = str(doc)
-        if len(sd) <= LIMIT:
-            print(sd, end='<br>')
-        else:
-            print(sd[:LIMIT], '...', end='<br>')
-
-    print('<hr>')
-    print('<pre>')
-    print(json.dumps(results.response.json(), indent=4))
-    print('</pre>')
-    print('<hr>')
-
+    C.print_p(C.t('Number of results found') + ': ' + str(results.numFound))
+    C.show_results(results)
+    results.debugprint()
 else:
-    C.print_header(title = 'CLIR: nebyl zadán žádný dotaz')
-    C.print_h1('Nebyl zadán žádný dotaz')
+    C.print_header(title = C.t('CLIR: no query was specified'))
+    C.print_h1(C.t('CLIR: no query was specified'))
 
 C.print_footer()
